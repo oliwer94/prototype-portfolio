1a93a9ed98c1034a954c5245b5bba28168965740
Oliver Arnits <oliwer94@gmail.com>
2017-12-11 23:13:26 +0100

    c-r fix

--- src/app/frogger/frogger.component.ts
+++ src/app/frogger/frogger.component.ts
@@ -7,28 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FroggerComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
 
   proto: any =
   {
-    name: "Frogger",
-    about: 
+    name: 'Frogger',
+    about:
 `The exercise was to modify and already
 existing game. We have decided to implement
 the time mechaninc from Super Hot into Frogger.
 So the cars have a slow movement but when the
-frog moves they speed up too. To try to make 
+frog moves they speed up too. To try to make
 the game more playable, we have decided that
 horizontal movement is allowed without it affecting
 the speed of the cars.  `,
-    imgsrc: "\\assets\\images\\frogger.png",
-    pdf: "http://www.oliverarnits.com\\assets\\pdf\\Frogger.pdf",
-    tag: "frogger",
-    build:"\\assets\\builds\\SuperFrogger.zip"
+    imgsrc: '\\assets\\images\\frogger.png',
+    pdf: 'http://www.oliverarnits.com\\assets\\pdf\\Frogger.pdf',
+    tag: 'frogger',
+    build: '\\assets\\builds\\SuperFrogger.zip'
   };
+  constructor() { }
 
+  ngOnInit() {
+  }
 
 }
